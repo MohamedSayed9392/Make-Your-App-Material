52f07663227f648d878fdd2defe857b183c36445
MohamedSayed <MohamedSayed9392@gmail.com>
2017-07-15 00:07:49 +0200

    - first commit

---
+++ XYZReader/src/main/java/com/example/xyzreader/remote/RemoteEndpointUtil.java
@@ -0,0 +1,67 @@
+package com.example.xyzreader.remote;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONTokener;
+
+import java.io.IOException;
+import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class RemoteEndpointUtil {
+    private static final String TAG = "RemoteEndpointUtil";
+
+    private RemoteEndpointUtil() {
+    }
+
+    public static JSONArray fetchJsonArray() {
+        String itemsJson = null;
+        try {
+            itemsJson = fetchPlainText(Config.BASE_URL);
+            logLongLine(TAG,itemsJson);
+        } catch (IOException e) {
+            Log.e(TAG, "Error fetching items JSON", e);
+            return null;
+        }
+
+        // Parse JSON
+        try {
+            JSONTokener tokener = new JSONTokener(itemsJson);
+            Object val = tokener.nextValue();
+            if (!(val instanceof JSONArray)) {
+                throw new JSONException("Expected JSONArray");
+            }
+            return (JSONArray) val;
+        } catch (JSONException e) {
+            Log.e(TAG, "Error parsing items JSON", e);
+        }
+
+        return null;
+    }
+
+    static String fetchPlainText(URL url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    public static void logLongLine(String TAG,String veryLongString){
+        int maxLogSize = 1000;
+        for(int i = 0; i <= veryLongString.length() / maxLogSize; i++) {
+            int start = i * maxLogSize;
+            int end = (i+1) * maxLogSize;
+            end = end > veryLongString.length() ? veryLongString.length() : end;
+            Log.v(TAG, veryLongString.substring(start, end));
+        }
+    }
+}
